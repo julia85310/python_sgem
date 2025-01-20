0b6a7b76856e482e6fab7bce47b0c3022beda136
julia85310 <148855799+julia85310@users.noreply.github.com>
2025-01-20 14:43:44 +0100

    ej8

---
+++ python_b1/ejercicios3.py
@@ -0,0 +1,11 @@
+
+def pedirEstudiantes():
+    estudiantes = []
+    while (True):
+        estudiante = {'nombre': '', 'calificacion': -1}
+        estudiante['nombre'] = input('Introduce el nombre de un estudiante (FIN para finalizar): ')
+        if ()
+def main():
+    estudiantes = pedirEstudiantes();
+
+main()
