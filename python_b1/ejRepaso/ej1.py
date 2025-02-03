225b4b4f7af074f96f92b104a35ecaff3f40a60d
julia85310 <148855799+julia85310@users.noreply.github.com>
2025-02-03 14:41:42 +0100

    ameba

---
+++ python_b1/ejRepaso/ej1.py
@@ -0,0 +1,29 @@
+def function(texto):
+    try:
+        int(texto)
+        return "Error: String esperado. Numero recibido"
+    except:
+        mayorContador = 1
+
+    recuentoPalabras = {}
+    recuentoPalabrasOrdenado = {}
+    palabras = texto.split()
+    for palabra in palabras:
+        if palabra not in recuentoPalabras:
+            apariciones = palabras.count(palabra)
+            if apariciones > mayorContador:
+                mayorContador = apariciones
+            recuentoPalabras.update({palabra: apariciones})
+    for i in range(1, mayorContador + 1):
+        for palabra, apariciones in recuentoPalabras.items():
+            if (apariciones == i):
+                recuentoPalabrasOrdenado.update({palabra: apariciones})
+    return recuentoPalabrasOrdenado
+
+texto = input("Introduce un texto: ")
+print(function(texto))
+
+
+
+
+    
